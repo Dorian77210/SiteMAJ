9e58b19fd998685f87ee78b03f83299a69d06d09
terbah <terbah@localhost.localdomain>
2018-11-03 17:05:24 +0000

    Commit

--- assets/JS/JQuery/src/openDiv.js
+++ assets/JS/JQuery/src/openDiv.js
@@ -1,17 +1,16 @@
-$(document).ready(function(){
-
-	let speed = 1500;
-	$('.openDiv').on('click', function(){
-
-		var arrow = '⬇️';
-		if($(this).text() === arrow) arrow = '➡️';
-		var span = $(this);
-		var div = $(this).parent('h5').parent('div').attr('id');
-		$('#' + div + ' .toOpen').fadeToggle(speed); //effet du paragraphe
-		$(this).fadeOut(speed / 2, function(){
-
-			$(this).fadeIn(speed / 2).text(arrow);
-		});
-
+let ACTIVE_COLOR = "#d8d8d4";
+let DEACTIVE_COLOR = "rgba(0, 0, 0, 0)";
+$(document).ready(function() {
+	$('.container').bind('click touchstart', function() {
+		$(this).parent('div').children('div').slideToggle(1500);
+		if($(this).css('background-color') == DEACTIVE_COLOR) {
+			$(this).css({
+				'background-color' 		: 		ACTIVE_COLOR
+			})
+		} else {
+			$(this).css({
+				'background-color' 		: 		DEACTIVE_COLOR
+			})
+		}
 	});
 });
